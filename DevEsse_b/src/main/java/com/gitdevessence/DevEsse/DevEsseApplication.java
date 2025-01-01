91069d8a98508f744bbc07026e35ce42a373988b
RRLL <samhuang1216@users.noreply.github.com>
2025-01-01 20:33:03 +0800

    init backend

---
+++ DevEsse_b/src/main/java/com/gitdevessence/DevEsse/DevEsseApplication.java
@@ -0,0 +1,13 @@
+package com.gitdevessence.DevEsse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevEsseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevEsseApplication.class, args);
+	}
+
+}
---
+++ DevEsse_b/src/test/java/com/gitdevessence/DevEsse/DevEsseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitdevessence.DevEsse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevEsseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
